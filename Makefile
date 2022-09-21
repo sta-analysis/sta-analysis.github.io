d4e3748d490f819252b9b4df29445347d2530d25
Alex Rutar <alex@rutar.org>
2022-09-21 15:56:54 +0100

    add new build script for website and update some quotes to make sed work better

--- Makefile
+++
@@ -1,7 +0,0 @@
-.PHONY: upload
-
-upload:
-	zola build
-	for name in archive postgraduate seminars links research members summer; do mv public/$$name/index.html public/$$name.html && rmdir public/$$name; done
-	rm public/404.html
-	rsync -a public analysis@twopi.mcs.st-andrews.ac.uk:old_webpage/
---
+++ build_mcs_site.fish
@@ -0,0 +1,14 @@
+#!/usr/bin/env fish
+
+
+zola build
+for name in archive postgraduate seminars links research members summer
+    mv public/$name/index.html public/$name.html
+    rmdir public/$name
+end
+rm -f public/404.html
+for name in archive postgraduate seminars links research members summer
+    sed -iE "s/\"\/$name\/\"/\"$name.html\"/g" public/*.html
+end
+sed -iE "s/\"\/\"/\"index.html\"/g" public/*.html
+sed -iE 's/="\//="/g' public/*.html
