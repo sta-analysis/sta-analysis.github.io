95c9e3d738b9113b6245c7896978782bb8f57275
Alex Rutar <alex@rutar.org>
2022-10-05 11:29:07 +0100

    update mcs build script

--- build_mcs_site.fish
+++ build_mcs_site.fish
@@ -1,15 +1,32 @@
+# build the directory
 zola build
+
+# remove folder nesting
 for name in archive postgraduate seminars links research members summer
     mv public/$name/index.html public/$name.html
     rmdir public/$name
 end
+
+# remove 404 page (automatically served)
 rm -f public/404.html
+
+# rename internal links to respect folder structure
 for name in archive postgraduate seminars links research members summer
     sed -i "s/\"\/$name\/\"/\"$name.html\"/g" public/*.html
 end
 sed -i "s/\"\/\"/\"index.html\"/g" public/*.html
 sed -i 's/="\//="/g' public/*.html
+
+# copy public directory remotely
 rsync -a public analysis@twopi.mcs.st-andrews.ac.uk:webpage/
+rsync -a public jmf32@twopi.mcs.st-andrews.ac.uk:public_html
+
+# move files to correct locations
 ssh analysis@twopi.mcs.st-andrews.ac.uk "mv ~/webpage/public/* ~/webpage/"
 ssh analysis@twopi.mcs.st-andrews.ac.uk "rmdir ~/webpage/public"
-echo "View updated site at: http://www.mcs.st-andrews.ac.uk/pg/pure/Analysis/seminars.html"
+ssh jmf32@twopi.mcs.st-andrews.ac.uk "mv ~/public_html/public ~/public_html/analysis"
+
+# display update information
+echo "Site updated at:"
+echo "  http://www.mcs.st-andrews.ac.uk/pg/pure/Analysis/index.html"
+echo "  http://www.mcs.st-andrews.ac.uk/~jmf32/analysis/index.html"
